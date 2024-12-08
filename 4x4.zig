44e4b701c63d64a00a71ce48b363df2f63fb9d4a
timosdev99 <timothyisah4@gmail.com>
2024-12-08 01:17:58 +0100

    4x4

--- 4x4.zig
+++ 4x4.zig
@@ -8,7 +8,7 @@ const mat4x4 = [4][4]f32{
     [_]f32{ 0.0, 0.0, 0.0, 1.0 },
 };
 test "multidimensional arrays" {
-    try expect(mat4x4[1][1] == 1.0);
+    try expect(mat4x4[0][1] == 0.0);
 
     for (mat4x4, 0..) |row, row_index| {
         for (row, 0..) |cell, column_index| {
