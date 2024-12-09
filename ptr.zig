5a814ab84eb5b1f102f34233927ce8b3d616c170
timosdev99 <timothyisah4@gmail.com>
2024-12-09 15:15:53 +0100

    ptr

--- ptr.zig
+++ ptr.zig
@@ -86,3 +86,23 @@ test "global variable alignment" {
     try expect(type_info.pointer.alignment == 4);
     try expect(@TypeOf(&foo) == *align(4) u8);
 }
+
+fn derp() align(@sizeOf(usize) * 2) i32 {
+    return 1234;
+}
+fn noop1() align(1) void {}
+fn noop4() align(4) void {}
+
+test "function alignment" {
+    try expect(derp() == 1234);
+    try expect(@TypeOf(derp) == fn () i32);
+    try expect(@TypeOf(&derp) == *align(@sizeOf(usize) * 2) const fn () i32);
+
+    noop1();
+    try expect(@TypeOf(noop1) == fn () void);
+    try expect(@TypeOf(&noop1) == *align(1) const fn () void);
+
+    noop4();
+    try expect(@TypeOf(noop4) == fn () void);
+    try expect(@TypeOf(&noop4) == *align(4) const fn () void);
+}
