045ad5da0692953c4aa003b2399b8fe37a06be3b
timosdev99 <timothyisah4@gmail.com>
2024-12-19 18:59:27 +0100

    linked list

--- struct.zig
+++ struct.zig
@@ -44,3 +44,45 @@ test "struct namespaced variable" {
 
     _ = does_nothing;
 }
+
+// in Zig:
+fn LinkedList(comptime T: type) type {
+    return struct {
+        pub const Node = struct {
+            prev: ?*Node,
+            next: ?*Node,
+            data: T,
+        };
+
+        first: ?*Node,
+        last: ?*Node,
+        len: usize,
+    };
+}
+
+test "linked list" {
+    try expect(LinkedList(i32) == LinkedList(i32));
+
+    const list = LinkedList(i32){
+        .first = null,
+        .last = null,
+        .len = 0,
+    };
+    try expect(list.len == 0);
+
+    const ListOfInts = LinkedList(i32);
+    try expect(ListOfInts == LinkedList(i32));
+
+    var node = ListOfInts.Node{
+        .prev = null,
+        .next = null,
+        .data = 1234,
+    };
+    const list2 = LinkedList(i32){
+        .first = &node,
+        .last = &node,
+        .len = 1,
+    };
+
+    try expect(list2.first.?.data == 1234);
+}
